63b9927ed700d11e90d4e14b447d657a014ad181
Sit, Kui Sing <kuisit@gatech.edu>
2015-11-21 17:40:21 -0500

    Updated Test to have qr solve as part of the test

--- Test.java
+++ Test.java
@@ -10,20 +10,21 @@ public class Test {
         test.set(2,1,3);
         test.set(3,1,4);
         test.set(0,2,1);
-        test.set(1,2,3);
-        test.set(2,2,6);
-        test.set(3,2,10);
-        test.set(0,3,1);
-        test.set(1,3,4);
-        test.set(2,3,10);
+        test.set(1, 2, 3);
+        test.set(2, 2, 6);
+        test.set(3, 2, 10);
+        test.set(0, 3, 1);
+        test.set(1, 3, 4);
+        test.set(2, 3, 10);
         test.set(3, 3, 20);
         System.out.print("Original:");
-        test.print(4, 4);
-        Givens.solve_qr_givens(test);
-        System.out.print("Q:");
-        Givens.getQ().print(4, 4);
-        System.out.print("R:");
-        Givens.getR().print(4, 4);
-        System.out.println("Error: " + Givens.getError());
+        test.print(7, 7);
+        Givens.qr_fact_givens(test);
+        Matrix b = new Matrix(new double [4][1]);
+        b.set(0,0,1);
+        b.set(1,0,(double) 1 / 2);
+        b.set(2,0,(double) 1 / 3);
+        b.set(3,0,(double) 1 / 4);
+        Givens.solve_qr_b(test, b);
     }
 }
