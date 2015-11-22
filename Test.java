7279398de2f7387808c74773c25e62e0f93c4bd1
Sit, Kui Sing <kuisit@gatech.edu>
2015-11-22 16:51:19 -0500

    Updated to have Jacobi tests

--- Test.java
+++ Test.java
@@ -1,15 +1,17 @@
+import java.util.Random;
+
 public class Test {
     public static void main(String[] args) {
         Matrix test = new Matrix(new double[4][4]);
-        test.set(0,0,1);
-        test.set(1,0,1);
-        test.set(2,0,1);
-        test.set(3,0,1);
-        test.set(0,1,1);
-        test.set(1,1,2);
-        test.set(2,1,3);
-        test.set(3,1,4);
-        test.set(0,2,1);
+        test.set(0, 0, 1);
+        test.set(1, 0, 1);
+        test.set(2, 0, 1);
+        test.set(3, 0, 1);
+        test.set(0, 1, 1);
+        test.set(1, 1, 2);
+        test.set(2, 1, 3);
+        test.set(3, 1, 4);
+        test.set(0, 2, 1);
         test.set(1, 2, 3);
         test.set(2, 2, 6);
         test.set(3, 2, 10);
@@ -21,10 +23,40 @@ public class Test {
         test.print(7, 7);
         Givens.qr_fact_givens(test);
         Matrix b = new Matrix(new double [4][1]);
-        b.set(0,0,1);
-        b.set(1,0,(double) 1 / 2);
-        b.set(2,0,(double) 1 / 3);
-        b.set(3,0,(double) 1 / 4);
+        b.set(0, 0, 1);
+        b.set(1, 0, (double) 1 / 2);
+        b.set(2, 0, (double) 1 / 3);
+        b.set(3, 0, (double) 1 / 4);
         Givens.solve_qr_b(test, b);
+
+        Matrix a = new Matrix(3, 3);
+        a.set(0, 0, 1);
+        a.set(1, 0, (double) 1 / 2);
+        a.set(2, 0, (double) 1 / 3);
+        a.set(0, 1, (double) 1 / 2);
+        a.set(1, 1, 1);
+        a.set(2, 1, (double) 1 / 4);
+        a.set(0, 2, (double) 1 / 3);
+        a.set(1, 2, (double) 1 / 4);
+        a.set(2, 2, 1);
+        Matrix c = new Matrix(3, 1);
+        c.set(0, 0, 0.1);
+        c.set(1, 0, 0.1);
+        c.set(2, 0, 0.1);
+        Random randNum = new Random();
+        Matrix approxTotal = new Matrix(3,1);
+        for (int count = 0; count < 100; count++) {
+            Matrix random = new Matrix(3, 1);
+            for (int i = 0; i < 3; i++) {
+                random.set(i, 0, randNum.nextDouble() - randNum.nextDouble());
+            }
+            approxTotal.plus(Jacobi.jacobi_iter(a, c, random, 0.00005, 100));
+        }
+        Matrix actualTotal = new Matrix(3,1);
+        actualTotal.set(0, 0, (double) 9 / 190);
+        actualTotal.set(1, 0, (double) 28 / 475);
+        actualTotal.set(2, 0, (double) 33 / 475);
+        System.out.println("");
+        Jacobi.iterativeError(approxTotal, actualTotal);
     }
 }
