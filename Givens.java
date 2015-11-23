42ddf4a83f8d0ff014bc038eb513e9717266503b
Sit, Kui Sing <kuisit@gatech.edu>
2015-11-23 17:20:47 -0500

    Update Givens.java

--- Givens.java
+++ Givens.java
@@ -117,4 +117,32 @@ public class Givens {
             return null;
         }
     }
+    
+    public static void main(String[] args) {
+        Matrix test = new Matrix(new double[4][4]);
+        test.set(0, 0, 1);
+        test.set(1, 0, 1);
+        test.set(2, 0, 1);
+        test.set(3, 0, 1);
+        test.set(0, 1, 1);
+        test.set(1, 1, 2);
+        test.set(2, 1, 3);
+        test.set(3, 1, 4);
+        test.set(0, 2, 1);
+        test.set(1, 2, 3);
+        test.set(2, 2, 6);
+        test.set(3, 2, 10);
+        test.set(0, 3, 1);
+        test.set(1, 3, 4);
+        test.set(2, 3, 10);
+        test.set(3, 3, 20);
+        Givens.qr_fact_givens(test);
+        Matrix b = new Matrix(new double [4][1]);
+        b.set(0, 0, 1);
+        b.set(1, 0, (double) 1 / 2);
+        b.set(2, 0, (double) 1 / 3);
+        b.set(3, 0, (double) 1 / 4);
+        Givens.solve_qr_b(test, b);
+    }
+    }
 }
